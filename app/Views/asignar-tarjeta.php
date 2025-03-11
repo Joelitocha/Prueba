dff13b6f431d71c7f50285e25bdf39aa3673ecc8
joel <joelmartinez@alumnos.itr3.edu.ar>
2025-03-11 10:18:34 -0300

    ananananashe

--- app/Views/asignar-tarjeta.php
+++ app/Views/asignar-tarjeta.php
@@ -21,7 +21,7 @@
     <form action="/asignar-tarjeta-usuario" method="POST">
       <label for="id_usuario">ID del Usuario:</label>
       <input type="text" id="id_usuario" name="id_usuario">
-      <br><br>
+      <br><br> 
       <label for="id_tarjeta">ID de la Tarjeta:</label>
       <input type="text" id="id_tarjeta" name="id_tarjeta">
       <br><br>
