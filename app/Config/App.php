e9909501ca309a045231cf06bcb27b2e1dae8754
GEORGE2025-GORDO <federicoarias@alumnos.itr3.edu.ar>
2025-03-19 10:13:12 -0300

    ndewaaaaaaaaaaaaa

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public $baseURL = 'http://localhost/cosas/Prueba/public';
+    public $baseURL = 'http://l192.168.2.194/cosas/Prueba/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
