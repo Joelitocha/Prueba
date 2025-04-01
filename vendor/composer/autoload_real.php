d629cf296991edc4e2eb92961dd54688ec73a327
Ec2 <RackOnOficial@gmail.com>
2025-04-01 21:28:19 +0000

    hola

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,29 +52,6 @@ class ComposerAutoloaderInitdec90ba52f9bfafc250e5f86bc402c6a
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitdec90ba52f9bfafc250e5f86bc402c6a::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequiredec90ba52f9bfafc250e5f86bc402c6a($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequiredec90ba52f9bfafc250e5f86bc402c6a($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
