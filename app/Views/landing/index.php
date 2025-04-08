68196b99e921f0528240a05760e6c950bea9a27e
Joelitocha <joelitochanderfu@gmail.com>
2025-04-08 19:30:55 -0300

    gwgwgwg

--- app/Views/landing/index.php
+++ app/Views/landing/index.php
@@ -489,7 +489,7 @@
     <nav id="navbar" class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="assets/images/RackON.png" alt="RackON Logo" style="height: 40px;">
+                <img src="assets/images/pro.png" alt="RackON Logo" style="height: 40px;">
             </a>
             
             <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
