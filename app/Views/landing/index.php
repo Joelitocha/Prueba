0d7798d2471430849e0b9252f6e343906cda895a
joel <joelmartinez@alumnos.itr3.edu.ar>
2025-04-10 09:45:50 -0300

    wgwergerg

--- app/Views/landing/index.php
+++ app/Views/landing/index.php
@@ -656,7 +656,7 @@
                 <!-- Columna de Imagen -->
                 <div class="col-lg-6 order-lg-1 order-2 image-column">
                     <div class="h-100 w-100 overflow-hidden">
-                        <img src="assets/images/details-background.jpg" 
+                        <img src="assets/images/pro.png" 
                              alt="Tecnología RackON en acción"
                              class="img-fluid w-100 h-100 object-fit-cover">
                         <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center" style="background: rgba(0,0,0,0.3);">
