edcab434eee2382819ed13099bf618ecec090f9d
Joelitocha <joelitochanderfu@gmail.com>
2025-04-11 20:33:31 -0300

    jtyjtyjfgj

--- app/Views/landing/index.php
+++ app/Views/landing/index.php
@@ -149,7 +149,7 @@
     
     /* HEADER */
     #header {
-        background: linear-gradient(rgba(21, 35, 63, 0.7), rgba(21, 35, 63, 0.7)), url('assets/images/rackon-og.jpg') center center no-repeat;
+        background: linear-gradient(rgba(21, 35, 63, 0.7), rgba(21, 35, 63, 0.7)), url('assets/images/princ-img.jpg') center center no-repeat;
         background-size: cover;
         color: white;
         text-align: center;
@@ -556,7 +556,7 @@
     <nav id="navbar" class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container">
             <a class="navbar-brand" href="">
-                <img src="assets/images/princ-img.jpg" alt="RackON Logo" style="height: 40px;">
+                <img src="assets/images/pro.png" alt="RackON Logo" style="height: 40px;">
             </a>
             
             <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-expanded="false" aria-label="Toggle navigation">
