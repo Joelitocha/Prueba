355cb586a8bc00c0bade660cb47d9f50c9857a48
joel <joelmartinez@alumnos.itr3.edu.ar>
2025-06-26 08:43:02 -0300

    ñañañañ

--- app/Views/landing/index.php
+++ app/Views/landing/index.php
@@ -560,34 +560,58 @@ $session=session();
 </head>
 <body>
     
-    <!-- Navigation -->
-    <nav id="navbar" class="navbar navbar-expand-lg navbar-dark fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="">
-                <img src="assets/images/pro.png" alt="RackON Logo" style="height: 40px;">
-            </a>
-            
-            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse" id="navbarContent">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link active" href="#header">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#introduction">¿Qué es RackON?</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#funcionamiento">¿Cómo funciona?</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#hardware">Hardware</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#planes">Planes</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#contact">Contáctenos</a></li>
-                    <li class="nav-item ms-lg-3 my-2 my-lg-0">
-                        <a class="btn btn-outline-light" href="login">
-                            <i class="fas fa-sign-in-alt me-2"></i>Acceder
-                        </a>
-                    </li>
-                </ul>
-            </div>
+<!-- Navigation -->
+<nav id="navbar" class="navbar navbar-expand-lg navbar-dark fixed-top">
+    <div class="container">
+        <!-- Logo optimizado con tamaño responsivo -->
+        <a class="navbar-brand" href="#header">
+            <img src="assets/images/pro.png" alt="RackON Logo" class="img-fluid" style="height: 40px; max-width: 120px;">
+        </a>
+        
+        <!-- Botón hamburguesa mejorado -->
+        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" 
+                aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <!-- Menú principal con mejor estructura -->
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#header" aria-current="page">Inicio</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        Producto
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#introduction">¿Qué es RackON?</a></li>
+                        <li><a class="dropdown-item" href="#funcionamiento">¿Cómo funciona?</a></li>
+                        <li><a class="dropdown-item" href="#hardware">Hardware</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#planes">Planes y Precios</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contacto</a>
+                </li>
+                <li class="nav-item ms-lg-3 my-2 my-lg-0">
+                    <a class="btn btn-outline-light" href="login">
+                        <i class="fas fa-sign-in-alt me-2"></i>Acceder
+                    </a>
+                </li>
+                <li class="nav-item d-lg-none mt-2">
+                    <div class="social-icons d-flex justify-content-center">
+                        <a href="#" class="btn btn-outline-light btn-sm rounded-circle me-2"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="btn btn-outline-light btn-sm rounded-circle me-2"><i class="fab fa-twitter"></i></a>
+                        <a href="#" class="btn btn-outline-light btn-sm rounded-circle"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
     <!-- Header -->
     <section id="header" class="fullpage-section">
@@ -1067,8 +1091,8 @@ $session=session();
   },
   "offers": {
     "@type": "Offer",
-    "priceCurrency": "MXN",
-    "price": "1999.00",
+    "priceCurrency": "ARS",
+    "price": "500000.00",
     "availability": "https://schema.org/InStock",
     "url": "https://rackon.tech/"
   }
