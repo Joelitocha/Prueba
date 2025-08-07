af53d1c58d3020ce0ac6653e8e0c9161b67aa91a
joel <joelmartinez@alumnos.itr3.edu.ar>
2025-08-07 08:18:51 -0300

    jhj

--- app/Views/login.php
+++ app/Views/login.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- ========== ESTILOS ========== -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>Inicio de Sessión</title>
     <style>
     body {
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
