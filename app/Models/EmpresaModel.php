f81187315b0b8b53dbb42a5aa921fb288727bb1e
joel <joelmartinez@alumnos.itr3.edu.ar>
2025-09-25 14:11:52 -0300

    Creación del nuevo modelo para la empresa

---
+++ app/Models/EmpresaModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class EmpresaModel extends Model
+{
+    protected $table            = 'empresa';
+    protected $primaryKey       = 'id_empresa';
+    protected $useAutoIncrement = true;
+
+    protected $allowedFields    = ['nombre', 'Ecode'];
+    public    $timestamps       = false; // tu tabla no tiene created_at / updated_at
+
+    // Validación simple
+    protected $validationRules = [
+        'nombre' => 'required|max_length[100]'
+    ];
+    private function generateEcode(): string
+{
+    return strtoupper(substr(bin2hex(random_bytes(6)), 0, 12)); 
+    // ejemplo: "A1B2C3D4E5F6" 12 caracteres.
+}
+
+}
