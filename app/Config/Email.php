f6f50086c571aa6a5698e414093432137c9a7804
Joel_Martinez_Vilche <joelmartinez@alumnos.itr3.edu.ar>
2025-10-29 21:55:49 -0300

    fdsfsd

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -11,48 +11,111 @@ class Email extends BaseConfig
     public string $recipients = '';
 
     /**
-     * Protocolo de envío
+     * The "user agent"wet
+     */
+    public string $userAgent = 'CodeIgniter';
+
+    /**
+     * The mail sending protocol: mail, sendmail, smtp
      */
     public string $protocol = 'smtp';
 
     /**
-     * Servidor SMTP de Amazon SES
+     * The server path to Sendmail.
      */
-    public string $SMTPHost = 'email-smtp.sa-east-1.amazonaws.com'; // Región: Sudamérica (São Paulo)
+    public string $mailPath = '/usr/sbin/sendmail';
 
     /**
-     * Credenciales SMTP de Amazon SES
+     * SMTP Server Hostname
      */
-    public string $SMTPUser = 'AKIAUJQ7RSKIDELQORXV'; // tu "Nombre de usuario de SMTP"
-    public string $SMTPPass = 'BNLhm3MWtmovSnuEEjLqQIiTJXW9aCLPfslOQ02f1/U'; // tu "Contraseña SMTP"
+    public string $SMTPHost = 'email-smtp.sa-east-1.amazonaws.com';
 
     /**
-     * Puerto y cifrado
+     * SMTP Username
      */
-    public int $SMTPPort = 587; // STARTTLS
+    public string $SMTPUser = 'AKIAUJQ7RSKIDELQORXV';
+
+    /**
+     * SMTP Password
+     */
+    public string $SMTPPass = 'BNLhm3MWtmovSnuEEjLqQIiTJXW9aCLPfslOQ02f1/U';
+
+    /**
+     * SMTP Port
+     */
+    public int $SMTPPort = 587;
+
+    /**
+     * SMTP Timeout (in seconds)
+     */
+    public int $SMTPTimeout = 5;
+
+    /**
+     * Enable persistent SMTP connections
+     */
+    public bool $SMTPKeepAlive = false;
+
+    /**
+     * SMTP Encryption.
+     *
+     * @var string '', 'tls' or 'ssl'. 'tls' will issue a STARTTLS command
+     *             to the server. 'ssl' means implicit SSL. Connection on port
+     *             465 should set this to ''.
+     */
     public string $SMTPCrypto = 'tls';
 
     /**
-     * Configuración general
+     * Enable word-wrap
      */
-    public int $SMTPTimeout = 10;
-    public bool $SMTPKeepAlive = false;
-
     public bool $wordWrap = true;
+
+    /**
+     * Character count to wrap at
+     */
     public int $wrapChars = 76;
 
+    /**
+     * Type of mail, either 'text' or 'html'
+     */
     public string $mailType = 'html';
+
+    /**
+     * Character set (utf-8, iso-8859-1, etc.)
+     */
     public string $charset = 'utf-8';
+
+    /**
+     * Whether to validate the email address
+     */
     public bool $validate = false;
+
+    /**
+     * Email Priority. 1 = highest. 5 = lowest. 3 = normal
+     */
     public int $priority = 3;
 
+    /**
+     * Newline character. (Use “\r\n” to comply with RFC 822)
+     */
     public string $CRLF = "\r\n";
+
+    /**
+     * Newline character. (Use “\r\n” to comply with RFC 822)
+     */
     public string $newline = "\r\n";
 
+    /**
+     * Enable BCC Batch Mode.
+     */
     public bool $BCCBatchMode = false;
+
+    /**
+     * Number of emails in each BCC batch
+     */
     public int $BCCBatchSize = 200;
 
+    /**
+     * Enable notify message from server
+     */
     public bool $DSN = false;
 }
-
-
